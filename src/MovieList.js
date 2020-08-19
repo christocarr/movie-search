d95d0b1188b03e93d824403f912a42464b5615af
christocarr <christocarr@outlook.com>
2020-08-19 17:44:14 +0100

    Modify styling for larger screen sizes

--- src/MovieList.js
+++ src/MovieList.js
@@ -13,9 +13,13 @@ const MovieList = (props) => {
         return (
           <li className="movie__list-item" key={index}>
             <h3 className="movie__title">{movie.title}</h3>
+            <div className="movie__details">
+              <p><small>Release date: {movie.release_date}</small></p>
+              <p><small>Rating: {movie.vote_average}</small></p>
+            </div>
             {movie.backdrop_path ?
               <img className="movie__image" src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} alt={`${movie.title}`} />
-              : <div>No Image to Show</div>
+              : <div className="movie__image no-image">No Image to Show</div>
             }
             <p className="movie__desc">{movie.overview}</p>
           </li>
